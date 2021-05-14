d6246b95743ccb95a615c04bc659f784bb50007c
Alberto Sandoval <albertosand8@gmail.com>
2021-05-14 18:38:13 -0500

    changes again

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-'use strict'
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -6,11 +5,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //MIDDDLEWARES
-//
 app.use(express.json());
-app.use(express.static(__dirname+"/public"));
-app.use(cors());
-
+app.use(express.static(__dinarme+"/public"));
+app.use(cors())
 //Import routes
 const userRoute = require('./routes/user-route');
 const productRoute = require('./routes/product-route');
