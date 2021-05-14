ebae855c2e9805ac833eb9206a78e0a09fa38797
Alberto Sandoval <albertosand8@gmail.com>
2021-05-14 06:02:18 -0500

    third  change in app file, dirname and character

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const path = require('path')
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
 //MIDDDLEWARES
 app.use(express.json());
 app.use(express.static(__dirname+"/public"));
-app.use(cors())
+app.use(cors());
 //Import routes
 const userRoute = require('./routes/user-route');
 const productRoute = require('./routes/product-route');
